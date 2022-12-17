2b7b79e2dd04a2ec1e0053b7dbbcfe48dafb2416
j0eTheRipper <j0eTheRipper0010@gmail.com>
2022-12-17 19:42:50 +0300

    added new block colours

--- Block.py
+++ Block.py
@@ -1,25 +1,28 @@
 import pygame
 
-SPEED = 10
-
 
 class Block:
     def __init__(self, image, place):
         self.surface = pygame.image.load(image).convert_alpha()
         self.perimeter = self.surface.get_rect(center=place)
-
-    def rise(self, limit):
-        if self.perimeter.midtop[1] > limit:
-            self.perimeter.y -= SPEED
+        self.image = image
+        self.__speed = 5
 
     def get_down(self, limit):
         if self.perimeter.midbottom[1] < limit:
-            self.perimeter.y += SPEED
+            self.perimeter.y += self.__speed
 
     def go_left(self, limit):
         if self.perimeter.midleft[0] > limit:
-            self.perimeter.x -= SPEED
+            self.perimeter.x -= self.__speed
 
     def go_right(self, limit):
         if self.perimeter.midright[0] < limit:
-            self.perimeter.x += SPEED
+            self.perimeter.x += self.__speed
+
+    def move(self, max_limit):
+        self.get_down(max_limit)
+        if pygame.key.get_pressed()[pygame.K_LEFT]:
+            self.go_left(0)
+        elif pygame.key.get_pressed()[pygame.K_RIGHT]:
+            self.go_right(max_limit)
--- main.py
+++ main.py
@@ -1,42 +1,69 @@
 import pygame
 from Block import Block
 from sys import exit
+from random import randint
 
 
 pygame.init()
+
 MAX = 600
 screen = pygame.display.set_mode((MAX, MAX))
 clock = pygame.time.Clock()
 background = pygame.image.load('src/background.png').convert()
-blocks = [Block('src/block.png', (300, 0))]
-block = blocks[-1]
-print(block.perimeter.collidelist([i.perimeter for i in blocks[:-1]]))
+block_images = ('src/block.png', 'src/block1.png', 'src/block2.png')
+new_block = block_images[randint(0, 2)]
+blocks = [Block(new_block, (300, 0))]
 
 
-while True:
-    block = blocks[-1]
+def main():
+    while True:
+        check_for_events()
+
+        screen.blit(background, (0, 0))
+        render_blocks()
+        play_game()
+
+        pygame.display.update()
+        clock.tick(60)
+
+
+def check_for_events():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
-    screen.blit(background, (0, 0))
+
+def render_blocks():
     for i in blocks:
         screen.blit(i.surface, i.perimeter)
 
-    collidelist = block.perimeter.collidelist([i.perimeter for i in blocks[:-1]])
-    if block.perimeter.midbottom[1] <= MAX and collidelist == -1:
-        if pygame.key.get_pressed()[pygame.K_DOWN]:
-            block.get_down(MAX)
-        elif pygame.key.get_pressed()[pygame.K_LEFT]:
-            block.go_left(0)
-        elif pygame.key.get_pressed()[pygame.K_RIGHT]:
-            block.go_right(MAX)
-    elif block.perimeter.midtop <= 0:
+
+def play_game():
+    block = blocks[-1]
+    previous_blocks = block.perimeter.collidelist([i.perimeter for i in blocks[:-1]])
+
+    if block.perimeter.midbottom[1] < MAX and previous_blocks == -1:
+        block.move(MAX)
+    elif block.perimeter.midtop[1] <= 0:
         pygame.quit()
         exit()
     else:
-        blocks.append(Block('src/block.png', (300, 0)))
+        new_blk = choose_block(block)
+        blocks.append(Block(new_blk, (300, 0)))
 
-    pygame.display.update()
-    clock.tick(60)
+
+def choose_block(block):
+    if len(blocks) % 3 == 0:
+        return block_images[randint(0, 2)]
+    else:
+        if block.image == block_images[0]:
+            return block_images[1]
+        elif block.image == block_images[1]:
+            return block_images[2]
+        elif block.image == block_images[2]:
+            return block_images[0]
+
+
+if __name__ == '__main__':
+    main()
